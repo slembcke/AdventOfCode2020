de6fa05eba8e9c136126ca2b9ff0054dda2fc91e
slembcke <slembcke@gmail.com>
2020-12-01 13:40:17 -0600

    Two part answer apparently

--- day01.lua
+++ day01.lua
@@ -1,13 +1,35 @@
+dbg = require 'debugger'
+dbg.call(function()
+
 list = {}
-for line in io.lines("day01.txt") do
+for line in io.lines("day01.input") do
 	table.insert(list, tonumber(line))
 end
 
-for i = 1, #list do
-	for j = 1, #list do
-		if list[i] + list[j] == 2020 then
-			print(list[i]*list[j])
-			return
+function find2(list)
+	for _, a in ipairs(list) do
+		for _, b in ipairs(list) do
+			if a + b == 2020 then
+				print(a*b)
+				return
+			end
 		end
 	end
 end
+find2(list)
+
+function find3(list)
+	for _, a in ipairs(list) do
+		for _, b in ipairs(list) do
+			for _, c in ipairs(list) do
+				if a + b + c == 2020 then
+					print(a*b*c)
+					return
+				end
+			end
+		end
+	end
+end
+find3(list)
+
+end)
