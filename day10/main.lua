3ea428718e788053b7a59d9171421a99e60cef0a
slembcke <slembcke@gmail.com>
2020-12-10 00:34:08 -0600

    day 10

--- day10/main.lua
+++ day10/main.lua
@@ -3,38 +3,37 @@ loadfile "../common.lua"
 filename = ...
 f = io.open(filename)
 
--- dbg = require 'debugger'
-
 jolts = {}
 for line in f:lines() do table.insert(jolts, tonumber(line)) end
 
 table.sort(jolts)
-local diff1, diff3 = 1, 1
-for i = 2, #jolts do
-	local diff = jolts[i] - jolts[i - 1]
-	if diff == 1 then diff1 = diff1 + 1 end
-	if diff == 3 then diff3 = diff3 + 1 end
-end
 
-print(diff1*diff3)
+-- Part 1
+-- local diff1, diff3 = 1, 1
+-- for i = 2, #jolts do
+-- 	local diff = jolts[i] - jolts[i - 1]
+-- 	if diff == 1 then diff1 = diff1 + 1 end
+-- 	if diff == 3 then diff3 = diff3 + 1 end
+-- end
+-- print(diff1*diff3)
 
-local count = 0
-local sols = {}
-function chain_rec(idx0, jolt, sol)
-	if idx0 > #jolts then
-		-- print("solution", sol.." "..jolt)
-		count = count + 1
-		return
+local tmp, sums = {table.remove(jolts)}, {1}
+function push(jolt)
+	local sum = 0
+	for i = 1, #tmp do
+		if tmp[i] > jolt + 3 then break end
+		
+		sum = sum + sums[i]
 	end
 	
-	for i = idx0, #jolts do
-		if jolts[i] > jolt + 3 then break end
-		
-		chain_rec(i + 1, jolts[i], 0 --[[sol.." "..jolt]])
-	end
+	table.insert(tmp, 1, jolt)
+	table.insert(sums, 1, sum)
 end
 
--- dbg.call(function()
-chain_rec(1, 0, "")
-print(count)
--- end)
+while #jolts > 0 do
+	push(table.remove(jolts))
+end
+-- AURGH! I FORGOT ABOUT 0 FOR LIKE 30 MINUTES
+push(0)
+
+print(sums[1])
